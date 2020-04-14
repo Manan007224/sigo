49f6528e7b321c92b5936831c1c8b61c3301797b
Manan <mmaniyar@splunk.com>
2020-04-14 01:41:13 -0700

    add background jobs

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -15,8 +15,9 @@ import (
 )
 
 var (
-	clientContextClosed = fmt.Errorf("client context closed")
-	serverContextClosed = fmt.Errorf("server context closed")
+	clientContextClosed  = fmt.Errorf("client context closed")
+	serverContextClosed  = fmt.Errorf("server context closed")
+	oldestExceptableTime = 17
 )
 
 type Scheduler struct {
@@ -26,12 +27,7 @@ type Scheduler struct {
 	heartBeatMonitor *sync.Map
 
 	// Information of the connected clients. Just for logging purposes.
-	connectedClients *sync.Map
-
-	// contextMonitor is used for store separate contexts for each client and cancel that context
-	// so that other RPC's serving the same client could be stopped immediately.
-	contextMonitor map[net.Addr]context.Context
-	cancelMonitor  map[net.Addr]context.CancelFunc
+	connectedClients *map[string]bool
 
 	// schedulerCancelFunc and schedulerCtx are child context's of the server context so could be either used
 	// by the scheduler to cancel RPC execution of any other clients or could be cancelled by the server when
@@ -59,7 +55,7 @@ func NewScheduler(parentCtx context.Context) (*Scheduler, error) {
 	scheduler.mgr = mgr
 	scheduler.schedulerCtx, scheduler.schedulerCancelFunc = context.WithCancel(parentCtx)
 	scheduler.heartBeatMonitor = &sync.Map{}
-	scheduler.connectedClients = &sync.Map{}
+	scheduler.connectedClients = &map[string]bool{}
 
 	// Initialize the background jobs to be ran periodically.
 	jobs := []*Job{
@@ -69,6 +65,10 @@ func NewScheduler(parentCtx context.Context) (*Scheduler, error) {
 	}
 	scheduler.jobExecutor = NewJobsExecutor(jobs)
 
+	// Run all the background jobs
+	scheduler.jobExecutor.Run()
+	go scheduler.checkConnectedClients()
+
 	return scheduler, nil
 }
 
@@ -77,28 +77,10 @@ func (sc *Scheduler) getClientAddr(rpcContext context.Context) net.Addr {
 	return p.Addr
 }
 
-func (sc *Scheduler) getClientContextDone(rpcContext context.Context) <-chan struct{} {
-	return sc.contextMonitor[sc.getClientAddr(rpcContext)].Done()
-}
-
-// Close the client context from the server side, so any other RPC's serving the same client could
-// be cancelled.
-func (sc *Scheduler) closeClientContext(rpcContext context.Context) {
-	sc.cancelMonitor[sc.getClientAddr(rpcContext)]()
-}
-
-func (sc *Scheduler) setClientContext(rpcContext context.Context) {
-	childCtx, childCancel := context.WithCancel(sc.schedulerCtx)
-	sc.contextMonitor[sc.getClientAddr(rpcContext)] = childCtx
-	sc.cancelMonitor[sc.getClientAddr(rpcContext)] = childCancel
-}
-
 func (sc *Scheduler) checkClientOrServerContextClosed(rpcContext context.Context) error {
 	select {
 	case <-sc.schedulerCtx.Done():
 		return serverContextClosed
-	case <-sc.getClientContextDone(rpcContext):
-		return clientContextClosed
 	case <-rpcContext.Done():
 		return rpcContext.Err()
 	default:
@@ -107,9 +89,8 @@ func (sc *Scheduler) checkClientOrServerContextClosed(rpcContext context.Context
 }
 
 func (sc *Scheduler) Discover(context context.Context, clientConfig *pb.ClientConfig) (*pb.EmptyReply, error) {
-	sc.connectedClients.Store(clientConfig.Id, clientConfig)
+	(*sc.connectedClients)[sc.getClientAddr(context).String()] = true
 	sc.mgr.AddQueue(clientConfig.Queues...)
-	sc.setClientContext(context)
 	return &pb.EmptyReply{}, nil
 }
 
@@ -140,12 +121,9 @@ func (sc *Scheduler) HearBeat(stream pb.Scheduler_HeartBeatServer) error {
 
 			// client got shutdown entirely.
 			log.Printf("client: %s disconnected", sc.getClientAddr(stream.Context()).String())
-			// NOT sure if this is the right thing todo.
-			sc.closeClientContext(stream.Context())
-
 			return err
 		}
-		sc.heartBeatMonitor.Store(sc.getClientAddr(ctx), time.Now().Unix())
+		sc.heartBeatMonitor.Store(sc.getClientAddr(ctx).String(), time.Now().Unix())
 	}
 }
 
@@ -157,6 +135,9 @@ func (sc *Scheduler) Fetch(context context.Context, queue *pb.Queue) (*pb.JobPay
 }
 
 func (sc *Scheduler) Acknowledge(context context.Context, job *pb.JobPayload) (*pb.EmptyReply, error) {
+	if err := sc.checkClientOrServerContextClosed(context); err != nil {
+		return nil, err
+	}
 	sc.mgr.Do(func() error {
 		return sc.mgr.Acknowledge(job)
 	})
@@ -164,8 +145,41 @@ func (sc *Scheduler) Acknowledge(context context.Context, job *pb.JobPayload) (*
 }
 
 func (sc *Scheduler) Fail(context context.Context, failJob *pb.FailPayload) (*pb.EmptyReply, error) {
+	if err := sc.checkClientOrServerContextClosed(context); err != nil {
+		return nil, err
+	}
 	sc.mgr.Do(func() error {
 		return sc.mgr.Fail(failJob)
 	})
 	return &pb.EmptyReply{}, nil
+}
+
+func (sc *Scheduler) Shutdown() {
+	sc.mgr.Shutdown()
+	sc.jobExecutor.Shutdown()
+}
+
+func (sc *Scheduler) checkConnectedClients() {
+	ticker := time.NewTicker(15 * time.Second)
+	for range ticker.C {
+		select {
+		case <-sc.schedulerCtx.Done():
+			return
+		default:
+		}
+		exceptablePingTime := time.Now().Add(-time.Duration(oldestExceptableTime) * time.Second).Unix()
+		for client := range *(sc.connectedClients) {
+			lastPingTime, ok := sc.heartBeatMonitor.Load(client)
+			if !ok {
+				continue
+			}
+			if lastPingTime.(int64) < exceptablePingTime {
+				// TODO - also delete any ongoing RPC calls made by this client.
+				delete(*sc.connectedClients, client)
+				sc.heartBeatMonitor.Delete(client)
+
+				log.Printf("[CLIENT] %s disconnected", client)
+			}
+		}
+	}
 }
