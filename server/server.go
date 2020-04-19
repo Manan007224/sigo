2a9224db7bc2c47409da1e353f35a9a6ed07d372
Manan <mmaniyar@splunk.com>
2020-04-19 03:03:21 -0700

    add server gracefull shutdown

--- server/server.go
+++ server/server.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	pb "github.com/Manan007224/sigo/pkg/proto"
 	"github.com/Manan007224/sigo/pkg/scheduler"
@@ -22,10 +25,31 @@ func main() {
 
 	srv := grpc.NewServer()
 	ctx, cancel := context.WithCancel(context.Background())
-	scheduler, err := scheduler.NewScheduler(ctx)
+	scheduler, err := scheduler.NewScheduler(ctx, cancel)
 	if err != nil {
 		log.Fatalf("faild to create scheduler: %v", err)
 	}
-
 	pb.RegisterSchedulerServer(srv, scheduler)
+
+	srv.Serve(listener)
+
+	go gracefulShutdown(hookSignals(), cancel, srv, scheduler)
+
+}
+
+func hookSignals() chan os.Signal {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	return sigs
+}
+
+func gracefulShutdown(sigs chan os.Signal, cancel context.CancelFunc, srv *grpc.Server, sc *scheduler.Scheduler) {
+	<-sigs
+	cancel()
+	sc.Shutdown()
+	srv.GracefulStop()
+
+	log.Printf("finally shut everything")
+
+	os.Exit(1)
 }
